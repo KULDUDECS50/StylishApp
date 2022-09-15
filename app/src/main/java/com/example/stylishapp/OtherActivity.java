b2b17e524d9a4f9eee8afab01e556ab83707f943
Kuldeep <63112459+KULDUDECS50@users.noreply.github.com>
2022-09-15 12:54:47 -0500

    cool stuff

---
+++ app/src/main/java/com/example/stylishapp/OtherActivity.java
@@ -0,0 +1,14 @@
+package com.example.stylishapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class OtherActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_grid);
+    }
+}
