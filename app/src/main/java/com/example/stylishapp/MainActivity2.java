f4be9e1209a86898d3ce86118e3011a8096e643e
Kuldeep <63112459+KULDUDECS50@users.noreply.github.com>
2022-09-16 12:55:35 -0500

    try ficing the bug

--- app/src/main/java/com/example/stylishapp/MainActivity2.java
+++ app/src/main/java/com/example/stylishapp/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.stylishapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void switchScreens(View view) {
+        Button editText = findViewById(R.id.button);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
